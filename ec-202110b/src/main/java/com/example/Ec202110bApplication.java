4dd34ff215a30841a5882d6eb654580c37df1ba0
akiyoshi71515 <akiyoshi.71515@gmail.com>
2023-02-28 08:59:16 +0900

    ビルド前の編集

--- ec-202110b/src/main/java/com/example/Ec202110bApplication.java
+++ ec-202110b/src/main/java/com/example/Ec202110bApplication.java
@@ -2,12 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class Ec202110bApplication {
+public class Ec202110bApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ec202110bApplication.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(Ec202110bApplication.class, args);
+        }
+@Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(Ec202110bApplication.class);
+    }
+
 
 }
+
